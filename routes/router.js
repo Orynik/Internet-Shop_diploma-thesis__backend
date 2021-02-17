13d37dd24c96c34e86de3f4a8432f76da57b60db
Orynik <dkrovel@gmail.com>
2021-02-17 10:31:15 +0800

    Добавлена минимальная обработка ошибок * Процесс обработки ошибок Базы выведена в отдельную функцию

--- routes/router.js
+++ routes/router.js
@@ -1,56 +1,96 @@
 const {Router} = require('express')
 const formParser = require('formidable')
-const database = require('../database')
 const fs = require('fs')
 const router = Router()
-
 const colors = require('colors')
+
+const database = require('../database')
+const errorCode = require("../sqlError")
+const { resolveSoa } = require('dns')
 
 const directoryToSaveImagesProducts = `/home/orynik/Desktop/Projects/Internet-Shop_diploma-thesis__backend/images/products/`;
 const directoryToSaveImagesProductsWindows = `C:\\Users\\Orynik\\Desktop\\Диплом\\Internet-Shop_diploma-thesis__backend\\images\\products\\`;
+const URLImageServer = `http://localhost:4444/images/products`
 //TODO: Написать обработчики для обработки респонсов
+
+function checkErrorDB(mysqlErr,res){
+  for(const sqlErr in errorCode){
+    if(mysqlErr.code == sqlErr){
+      if(sqlErr == "ER_DATA_TOO_LONG"){
+        res.status(400).send(sqlErr).end()
+        break
+      }if(sqlErr == "ER_DUP_KEY"){
+        res.status(400).send(sqlErr).end()
+        break
+      }if(sqlErr.errono == "ER_TRUNCATED_WRONG_VALUE"){
+        console.log("done")
+        res.status(400).send(sqlErr).end()
+        break
+      }else{
+        console.log(sqlErr)
+        res.sendStatus(500)
+        break
+      }
+    }
+  }
+}
 
 //RESTful Serials table
 
 router.get('/serials',  async (req,res) => {
 
   if(req.query.id > 0 && req.query.id != undefined){
-    database.query(`select id,Serial from Serials where id = '${req.query.id}'`,function(err,result,fields){
-      if (err) throw new Error(err)
+    database.query(`select id,Serial from Serials where id = \'${req.query.id}\'`,function(err,result,fields){
+      if (err){
+        res.status(500).send(err)
+      }
 
       res.send(JSON.stringify(result))
     })
   }else{
     database.query('select id,Serial from Serials',function(err,result,fields){
-      if (err) throw new Error(err)
-
-      res.send(JSON.stringify(result))
+      if (err){
+        res.status(500).send(err)
+      }
+      res.status(200).send(JSON.stringify(result))
     })
   }
 
 })
 
 router.post('/serials',  async (req,res) => {
-  database.query(`insert into Serials(serial) values ('${req.body.Serial}')`,function(err,result,fields){
-    if (err) throw new Error(err)
-
-    res.sendStatus(201)
+  database.query(`insert into Serials(serial) values (\'${req.body.Serial}\')`,function(err,result,fields){
+    if (err){
+     checkErrorDB(err,res)
+    }else{
+      res.sendStatus(201)
+    }
   })
 })
 
 router.put('/serials', async (req,res) =>{
   database.query(`update serials set Serial = '${req.body.Serial}' where id = '${req.body.id}'`, function(err,result,fields){
-    if (err) throw new Error(err)
+    if (err){
+      console.log("\n")
+      for(var k in err){
+          console.log(`${k}`.red + `: ${err[k]}`)
+      }
+      console.log("\n")
+      res.sendStatus(400)
+    }
 
-    res.sendStatus(200)
+    res.sendStatus(202)
   })
 })
 
 router.delete('/serials', async (req,res) =>{
   database.query(`delete from Serials where id = '${req.query.id}'`, function(err,result,fields){
-    if (err) throw new Error(err)
-
-    res.sendStatus(204)
+    if (err){
+      console.log(err)
+      res.status(500).send("Ошибка: Некорректный id")
+    }else{
+      res.sendStatus(204)
+    }
   })
 })
 
@@ -58,14 +98,20 @@ router.delete('/serials', async (req,res) =>{
 
 router.get('/manufacturers',  async (req,res) => {
   if(req.query.id >= 0 && req.query.id != undefined){
-    database.query(`select id,Company,Country,Tel,Address from manufacturers where id = '${req.query.id}'`,function(err,result,fields){
-      if (err) throw new Error(err)
+    database.query(`select id,Company,Country,Tel,Email,ZipCode,Street,City,Building from manufacturers where id = '${req.query.id}'`,function(err,result,fields){
+        if (err){
+          console.log(err)
+          res.sendStatus(500)
+        }
 
       res.send(JSON.stringify(result))
     })
   }else{
-    database.query('select id,Company,Country,Tel,Address from manufacturers',function(err,result,fields){
-      if (err) throw new Error(err)
+    database.query('select id,Company,Country,Tel,Email,ZipCode,Street,City,Building from manufacturers',function(err,result,fields){
+      if (err){
+        console.log(err)
+        res.sendStatus(500)
+      }
 
       const requestData = JSON.stringify(result)
 
@@ -75,16 +121,25 @@ router.get('/manufacturers',  async (req,res) => {
 })
 
 router.post('/manufacturers',  async (req,res) => {
-  database.query(`insert into manufacturers(Company,Location,Tel) values ('${req.body.Company}','${req.body.Location}','${req.body.Tel}')`,function(err,result,fields){
-    if (err) throw new Error(err)
-    
-    res.sendStatus(201)
+  database.query(`insert into manufacturers(Company,Country,Tel,Email,ZipCode,Street,City,Building) values (\'${req.body.Company}\',\'${req.body.Country}\',\'${req.body.Tel}\',\'${req.body.Email}\',\'${req.body.ZipCode}\',\'${req.body.Street}\',\'${req.body.City}\',\'${req.body.Building}\')`,function(err,result,fields){
+    if (err){
+      checkErrorDB(err,res)
+    }else{
+      res.sendStatus(201)
+    }
   })
 })
 
 router.put('/manufacturers', async (req,res) =>{
-  database.query(`update manufacturers set Company = '${req.body.Company}', Location = '${req.body.Location}', Tel = '${req.body.Tel}' where id = '${req.body.id}'`, function(err,result,fields){
-    if (err) throw new Error(err)
+  database.query(`update manufacturers set Company = \'${req.body.Company}\', Country = \'${req.body.Country}\',Email = \'${req.body.Email}\',ZipCode = \'${req.body.ZipCode}\',Street = \'${req.body.City}\',City = \'${req.body.City}\', Building = \'${req.body.Building}\'  , Tel = \'${req.body.Tel}\' where id = ${req.body.id}`, function(err,result,fields){
+    if (err){
+      console.log("\n")
+      for(var k in err){
+          console.log(`${k}`.red + `: ${err[k]}`)
+      }
+      console.log("\n")
+      res.sendStatus(400)
+    }
 
     res.sendStatus(200)
   })
@@ -92,9 +147,12 @@ router.put('/manufacturers', async (req,res) =>{
 
 router.delete('/manufacturers', async (req,res) =>{
   database.query(`delete from manufacturers where id = '${req.query.id}'`, function(err,retult,fields){
-    if (err) throw new Error(err)
-
-    res.sendStatus(204)
+    if (err){
+      console.log(err)
+      res.status(500).send("Ошибка: Некорректный id")
+    }else{
+      res.sendStatus(204)
+    }
   })
 })
 
@@ -102,16 +160,22 @@ router.delete('/manufacturers', async (req,res) =>{
 
 router.get('/motors',  async (req,res) => {
   if(req.query.id > 0 && req.query.id != undefined){
-    database.query(`select id,Name,Serial,Power,RotationSpeed,Perfomance,PowerFactor, MultiplicityMaximum, Sliding from motors where id = ${req.query.id}`,function(err,result,fields){
-      if (err) throw new Error(err)
+    database.query(`select id,Name,Serial,OperatingVoltage,Power,RotationSpeed,Perfomance,PowerFactor, MultiplicityMaximum, Sliding from motors where id = ${req.query.id}`,function(err,result,fields){
+      if (err){
+        console.log(err)
+        res.sendStatus(500)
+      }
 
       const requestData = JSON.stringify(result)
 
       res.send(requestData)
     })
   }else{
-    database.query('select id,Name,Serial,Power,RotationSpeed,Perfomance,PowerFactor, MultiplicityMaximum, Sliding from motors',function(err,result,fields){
-      if (err) throw new Error(err)
+    database.query('select id,Name,Serial,OperatingVoltage,Power,RotationSpeed,Perfomance,PowerFactor, MultiplicityMaximum, Sliding from motors',function(err,result,fields){
+      if (err){
+        console.log(err)
+        res.sendStatus(500)
+      }
 
       const requestData = JSON.stringify(result)
 
@@ -121,29 +185,38 @@ router.get('/motors',  async (req,res) => {
 })
 
 router.post('/motors', async (req,res) =>{
-  console.log(`insert into motors(Name,Serial,Power,RotationSpeed,Perfomance, PowerFactor, MultiplicityMaximum, Sliding) values (\'${req.body.Name}\',\'${req.body.Serial}\',${req.body.Power},${req.body.RotationSpeed},${req.body.Perfomance},${req.body.PowerFactor},${req.body.MultiplicityMaximum},${req.body.Sliding})`.red)
-  database.query(`insert into motors(Name,Serial,Power,RotationSpeed,Perfomance, PowerFactor, MultiplicityMaximum, Sliding) values (\'${req.body.Name}\',\'${req.body.Serial}\',${req.body.Power},${req.body.RotationSpeed},${req.body.Perfomance},${req.body.PowerFactor},${req.body.MultiplicityMaximum},${req.body.Sliding})`, function(err,result,field){
-    if (err) throw new Error(err)
-
-    res.send(JSON.stringify(result))
+  database.query(`insert into motors(Name,Serial,OperatingVoltage,Power,RotationSpeed,Perfomance, PowerFactor, MultiplicityMaximum, Sliding) values (\'${req.body.Name}\',\'${req.body.Serial}\',${req.body.OperatingVoltage},${req.body.Power},${req.body.RotationSpeed},${req.body.Perfomance},${req.body.PowerFactor},${req.body.MultiplicityMaximum},${req.body.Sliding})`, function(err,result,field){
+    if (err){
+      checkErrorDB(err,res)
+    }else{
+      res.sendStatus(201)
+    }
   })
 })
 
 router.put('/motors', async (req,res) =>{
-  console.log(`update motors set Name = '${req.body.Name}', Serial = '${req.body.Serial}', Power = '${req.body.Power}' ,RotationSpeed = '${req.body.RotationSpeed}',Perfomance = ${req.body.Perfomance}, PowerFactor = ${req.body.PowerFactor},MultiplicityMaximum = ${req.body.MultiplicityMaximum},Sliding = ${req.body.Sliding} where id = ${req.query.id}`)
-
-  database.query(`update motors set Name = '${req.body.Name}', Serial = '${req.body.Serial}', Power = '${req.body.Power}' ,RotationSpeed = '${req.body.RotationSpeed}',Perfomance = ${req.body.Perfomance}, PowerFactor = ${req.body.PowerFactor} where id = ${req.query.id}`, function(err,result,fields){
-    if (err) throw new Error(err)
-
-    res.sendStatus(200)
+  database.query(`update motors set Name = '${req.body.Name}', Serial = '${req.body.Serial}', OperatingVoltage = '${req.body.OperatingVoltage}', Power = '${req.body.Power}', RotationSpeed = '${req.body.RotationSpeed}' ,Perfomance = ${req.body.Perfomance}, PowerFactor = ${req.body.PowerFactor} where id = ${req.body.id}`, function(err,result,fields){
+    if (err){
+      console.log("\n")
+      for(var k in err){
+          console.log(`${k}`.red + `: ${err[k]}`)
+      }
+      console.log("\n")
+      res.sendStatus(400)
+    }else{
+      res.sendStatus(200)
+    }
   })
 })
 
 router.delete('/motors', async (req,res) =>{
   database.query(`delete from motors where id = '${req.query.id}'`, function(err,retult,fields){
-    if (err) throw new Error(err)
-
-    res.sendStatus(204)
+    if (err){
+      console.log(err)
+      res.status(500).send("Ошибка: Некорректный id")
+    }else{
+      res.sendStatus(204)
+    }
   })
 })
 
@@ -151,7 +224,10 @@ router.delete('/motors', async (req,res) =>{
 
 router.get('/products', async (req,res) => {
   database.query('select id,Name,Serial,LintToImage,Manufacturer,Description,Price from Products',function(err,result,fields){
-    if (err) throw new Error(err)
+    if (err){
+      console.log(err)
+      res.sendStatus(500)
+    }
 
     const requestData = JSON.stringify(result)
 
@@ -188,21 +264,21 @@ router.post('/products',  async (req,res) => {
 });
 
 router.put('/products', async (req,res) =>{
-  // fs.copyFile(
-  //   files.file.path,
-  //   `${directoryToSaveImagesProductsWindows}${fields.Name}.jpg`,
-  //   (err) => {
-  //     if(err) throw err
-  //     console.log('file moved'.red)
-  //   }
-  // )
+  fs.copyFile(
+    files.file.path,
+    `${directoryToSaveImagesProductsWindows}${fields.Name}.jpg`,
+    (err) => {
+      if(err) throw err
+      console.log('file moved'.red)
+    }
+  )
 
   let form = new formParser.IncomingForm();
 
   form.parse(req, function(err, fields, files){
-    console.log(`update products set Name = '${fields.name}', Serial = '${fields.serial}', LintToImage = '${directoryToSaveImagesProductsWindows}${fields.name}.jpg' ,Manufacturer = '${fields.manufacturer}',Price = ${fields.price} where id = ${fields.id}`.red)
+    console.log(`update products set Name = '${fields.name}', Serial = '${fields.serial}', LintToImage = '${URLImageServer}${fields.name}.jpg' ,Manufacturer = '${fields.manufacturer}',Price = ${fields.price} where id = ${fields.id}`.red)
     if (err) throw new Error(err)
-    database.query(`update products set Name = '${fields.name}', Serial = '${fields.serial}', LintToImage = '${directoryToSaveImagesProductsWindows}${fields.name}.jpg' ,Manufacturer = '${fields.manufacturer}',Price = ${fields.price} where id = ${fields.id}`,
+    database.query(`update products set Name = '${fields.name}', Serial = '${fields.serial}', LintToImage = '${URLImageServer}${fields.name}.jpg' ,Manufacturer = '${fields.manufacturer}',Price = ${fields.price} where id = ${fields.id}`,
       function(err,result,fields){
         if (err) throw new Error(err)
 
@@ -212,11 +288,17 @@ router.put('/products', async (req,res) =>{
 })
 
 router.delete('/products', async (req,res) =>{
-  console.log(req.query.id)
   database.query(`delete from products where id = '${req.query.id}'`, function(err,retult,fields){
-    if (err) throw new Error(err)
-
-    res.sendStatus(204)
+    if (err){
+      console.log("\n")
+      for(var k in err){
+          console.log(`${k}`.red + `: ${err[k]}`)
+      }
+      console.log("\n")
+      res.sendStatus(400)
+    }else{
+      res.sendStatus(204)
+    }
   })
 })
 
---
+++ sqlError.js
@@ -0,0 +1,9 @@
+module.exports = 
+  DATABASE_ERRORS ={
+        "ER_DUP_KEY": 1022,
+        "ER_DATA_TOO_LONG": 1406,
+        "ER_NO_REFERENCED_ROW": 1217,
+        // Ошибка INSERT/UPDATE
+        "ER_BAD_FIELD_ERROR": 1054,
+        "ER_TRUNCATED_WRONG_VALUE": 1292
+  }
